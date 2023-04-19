92bf8a6db4cde65a1c2208e42f96003d0ba6252c
juank <juanprogram06@gmail.com>
2023-04-19 12:30:06 -0500

    Adicion de estructura de datos stack, con todods sus metodos

---
+++ stacks/stack.js
@@ -0,0 +1,52 @@
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    peek(){
+        return this.top;
+    }
+    pop(){
+        if(this.length ==0) return console.warn('There are not more elements in the stack');
+        if(this.length ==1){
+            this.bottom = null;
+        }
+        const newTop = this.top.next;
+        this.top = newTop
+        this.length--;
+        return this
+    }
+    push(value){
+        if(value != undefined){
+            const newNode = new Node(value);
+            if(this.length ===0){
+                this.bottom= newNode
+                this.top= newNode
+            }else{
+                const holdingPointer = this.top;
+                this.top = newNode;
+                this.top.next = holdingPointer;
+            }
+                this.length ++;
+                return this
+        }else{
+            console.warn('Please, send a correct param');
+        }
+
+    }
+}
+
+let myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
